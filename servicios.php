220cadeddd4a02ac4fbcc988bcabf27017b23574
J Eduardo Mena D <eduardo.cifer@gmail.com>
2020-11-26 00:09:47 -0600

    commit inicial

---
+++ servicios.php
@@ -0,0 +1,143 @@
+<?php 
+    $titulo = "Servicios";
+    require('includes/header.php');
+?>
+
+<main>
+    <!--? slider Area Start-->
+    <div class="slider-area slider-area2 slider-height2 position-relative" data-background="assets/img/hero/hero2.png">
+        <div class="slider-active">
+            <!-- Single Slider -->
+            <div class="single-slider">
+                <div class="slider-cap-wrapper">
+                    <div class="hero-caption">
+                        <h1 data-animation="fadeInLeft" data-delay=".4s">Services</h1>
+                        <!-- breadcrumb Start-->
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                <li class="breadcrumb-item"><a href="#">Services</a></li> 
+                            </ol>
+                        </nav>
+                    </div>
+
+                    <!-- breadcrumb End -->
+                    <div class="hero-img">
+                        <img src="assets/img/hero/h1_hero2.png" alt=""  data-animation="fadeInRight" data-transition-duration="5s">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--? Services Area Start -->
+    <section class="services-section section-padding30 fix">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-10 col-lg-10 col-md-12">
+                    <!-- Section Tittle -->
+                    <div class="section-tittle text-center mb-70">
+                        <span>Our Service</span>
+                        <h2>We bring ideas to life by combining years of experiences of our very talented team.</h2>
+                    </div> 
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-services single-services2 mb-30 section-over1 text-center">
+                        <div class="services-img">
+                            <img src="assets/img/gallery/team1.png" alt="">
+                            <div class="services-caption">
+                                <h3><a href="#">Business consulting</a></h3>
+                                <p>The automated process starts as soon <br>as your clothes go into the machine.</p>
+                                <a href="#" class="btn btn3">Make Appointment</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-services single-services2 mb-30 section-over1 text-center">
+                        <div class="services-img">
+                            <img src="assets/img/gallery/team2.png" alt="">
+                            <div class="services-caption">
+                                <h3><a href="#">Media planing</a></h3>
+                                <p>The automated process starts as soon <br>as your clothes go into the machine.</p>
+                                <a href="#" class="btn btn3">Make Appointment</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-services single-services2 mb-30 section-over1 text-center">
+                        <div class="services-img">
+                            <img src="assets/img/gallery/team3.png" alt="">
+                            <div class="services-caption">
+                                <h3><a href="#">Professional HR</a></h3>
+                                <p>The automated process starts as soon <br>as your clothes go into the machine.</p>
+                                <a href="#" class="btn btn3">Make Appointment</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-services single-services2 mb-30 section-over1 text-center">
+                        <div class="services-img">
+                            <img src="assets/img/gallery/team2.png" alt="">
+                            <div class="services-caption">
+                                <h3><a href="#">Business consulting</a></h3>
+                                <p>The automated process starts as soon <br>as your clothes go into the machine.</p>
+                                <a href="#" class="btn btn3">Make Appointment</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-services single-services2 mb-30 section-over1 text-center">
+                        <div class="services-img">
+                            <img src="assets/img/gallery/team1.png" alt="">
+                            <div class="services-caption">
+                                <h3><a href="#">Media planing</a></h3>
+                                <p>The automated process starts as soon <br>as your clothes go into the machine.</p>
+                                <a href="#" class="btn btn3">Make Appointment</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-services single-services2 mb-30 section-over1 text-center">
+                        <div class="services-img">
+                            <img src="assets/img/gallery/team3.png" alt="">
+                            <div class="services-caption">
+                                <h3><a href="#">Professional HR</a></h3>
+                                <p>The automated process starts as soon <br>as your clothes go into the machine.</p>
+                                <a href="#" class="btn btn3">Make Appointment</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!--Services Area End -->
+    <!--? Want To work -->
+    <section class="wantToWork-area section-bg2"  data-background="assets/img/gallery/section_bg01.png">
+        <div class="container">
+            <div class="wants-wrapper w-padding2">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-xl-7 col-lg-7 col-md-12">
+                        <div class="wantToWork-caption wantToWork-caption2">
+                            <h2>Need a consulting services?</h2>
+                            <p>The automated process starts as soon as your clothes go into the machine. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6">
+                        <a href="#" class="btn w-btn wantToWork-btn mr-20">Make an Appointment</a>
+                        <a href="#" class="btn2 w-btn wantToWork-btn"><i class="fas fa-phone"></i> (89) 673 378-309</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Want To work End -->
+</main>
+<?php require('includes/footer.php'); ?>
